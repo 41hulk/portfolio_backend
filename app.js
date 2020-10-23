8c21c9db4e8813dd1bbfb0358295bd3982277a96
Ntare Guy <gntare2@gmail.com>
2020-10-23 08:58:02 +0200

    imported the cors packeage in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors'
 import "./configs/database.js";
 import "dotenv/config.js"
 import postRouter from './Routes/postRoutes.js'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
